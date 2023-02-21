37fc2327f674600ae7c206cb788fdda791a9415a
Qiushi Yan <qiushi.yann@gmail.com>
2023-02-21 15:13:06 -0600

    inspect function signature

--- astrodown/utils.py
+++ astrodown/utils.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from inspect import signature
+from typing import Callable
 
 
 def file_ext(filename: str) -> str:
@@ -7,3 +9,12 @@ def file_ext(filename: str) -> str:
 
 def enpath(path: str | Path) -> Path:
     return Path(path) if isinstance(path, str) else path
+
+
+def get_doc(value: any) -> str:
+    return value.__doc__ if value.__doc__ else ""
+
+
+def inspect_function(f: Callable):
+    doc = get_doc(f)
+    return "\n".join([doc, f"{f.__name__}{str(signature(f))}"]).strip()
