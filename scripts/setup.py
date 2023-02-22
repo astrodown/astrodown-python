5a31e316e314491cae8f6830c0e374ac0020102f
Qiushi Yan <qiushi.yann@gmail.com>
2023-02-22 00:20:31 -0600

    update setup script

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,11 +1,13 @@
 import astrodown.js
 
+
+astrodown_js = astrodown_js.to_py()
 # astrodown_js is passed through javascript using pyodide.globals.set("astrodown_js", value)
-def set_global(name: str, value: str):
+def _set_global(name: str, value: str):
     astrodown.globals[name] = value
 
 
 [
-    set_global(export["name"], astrodown.js.load_export(export))
+    _set_global(export["name"], astrodown.js.load_export(export))
     for export in astrodown_js["exports"]
 ]
