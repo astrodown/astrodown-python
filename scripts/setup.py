3c1bdcef1299f4fe575be274c4736fb5758873e5
Qiushi Yan <qiushi.yann@gmail.com>
2023-02-22 00:36:38 -0600

    test old export script

--- scripts/setup.py
+++ scripts/setup.py
@@ -2,9 +2,10 @@ import astrodown.js
 
 
 astrodown_js = astrodown_js.to_py()
-for export in astrodown_js["exports"]:
-    print(export, astrodown.js.load_export(export))
-    globals()[export["name"]] = astrodown.js.load_export(export)
+print(astrodown_js)
+# for export in astrodown_js["exports"]:
+#     print(export, astrodown.js.load_export(export))
+#     globals()[export["name"]] = astrodown.js.load_export(export)
 
 # # astrodown_js is passed through javascript using pyodide.globals.set("astrodown_js", value)
 # astrodown_js = astrodown_js.to_py()
